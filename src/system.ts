cae647f55f30a5a26ac64b14bca65264fc99f6dd
1912147250 <77073523+1912147250@users.noreply.github.com>
2023-04-12 14:15:47 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -12,7 +12,7 @@ export const defaultSetting = {
 
 export const defaultMessage = `Powered by CheZ Vercel
 - 感谢各位的访问本站由4.12日起为demo站点↓
-- 由于本站维护成本较大并不太好负担本站依然免费使用如若出现问题请耐心等待或联系站长获取稳定版[联系站长](https://chez-1302520615.cos.ap-nanjing.myqcloud.com/tmp_f1d56f5ca34cab3245d2b48a2c84406dca1a5432c61ce397.jpg)
+- 由于本站维护成本较大 并不太好负担 本站依然免费使用 如若出现问题请耐心等待或联系站长获取稳定版[联系站长](https://chez-1302520615.cos.ap-nanjing.myqcloud.com/tmp_f1d56f5ca34cab3245d2b48a2c84406dca1a5432c61ce397.jpg)
 - 特感谢[5 OpenAI APl(gpt-3.5-turbo)]提供支
 - 开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
