5db4a5456cc412f60645fefbdd4aacf0b039a810
1912147250 <77073523+1912147250@users.noreply.github.com>
2023-04-16 23:38:14 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -1,1 +1,29 @@
+import type { Model } from "./types"
 
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: "",
+  model: "gpt-3.5-turbo" as Model
+}
+
+export const defaultMessage = `Powered by CheZ Vercel
+- 由于本站被人恶意刷取token，所以进行维护暂时关闭，后续开放时间请进入交流群[加入群聊](https://cznm-1302520615.cos.ap-nanjing.myqcloud.com/tmp_0a3e572d3aeb8f91f7027a6c4a7c2bf73fd0d9bc00f8c8dc.jpg)
+- 特感谢 [OpenAI API (gpt-3.5-turbo)]提供支持。
+- 本网站由快雪空空熊部署 
+- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
+
+export type Setting = typeof defaultSetting
+
+export const defaultResetContinuousDialogue = false
+
+export const defaultMaxInputTokens: Record<Model, number> = {
+  "gpt-3.5-turbo": 3072,
+  "gpt-4": 6144,
+  "gpt-4-32k": 24576
+}
+
+export const defaultModel: Model = "gpt-3.5-turbo"
