25691762e3d50780b1cb05a1e95e5f120c153be3
1912147250 <77073523+1912147250@users.noreply.github.com>
2023-04-16 23:49:17 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -10,7 +10,7 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `Powered by CheZ Vercel
+export const defaultMessage = `CheZai KuaiXue
 - 由于本站被人恶意刷取token，所以进行维护暂时关闭，后续开放时间请进入交流群[加入群聊](https://cznm-1302520615.cos.ap-nanjing.myqcloud.com/tmp_0a3e572d3aeb8f91f7027a6c4a7c2bf73fd0d9bc00f8c8dc.jpg)
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
