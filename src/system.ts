f3397014476f1e51cb3088c58c84bbca4150929d
1912147250 <77073523+1912147250@users.noreply.github.com>
2023-04-16 23:10:32 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -12,9 +12,7 @@ export const defaultSetting = {
 
 export const defaultMessage = `Powered by CheZ Vercel
 - 感谢各位的访问
-- 如若出现任何问题请联系站长[联系站长](https://chez-1302520615.cos.ap-nanjing.myqcloud.com/tmp_f1d56f5ca34cab3245d2b48a2c84406dca1a5432c61ce397.jpg)
-- 特感谢[ OpenAI APl(gpt-3.5-turbo)]提供支持
-- 开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
+- 由于有人恶意刷取本站token，所以进行维护暂时关闭，后续开放时间请扫码上方二维码进入交流群
 
 export type Setting = typeof defaultSetting
 
